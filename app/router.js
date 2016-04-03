eded48a9be6d74de42340988443a0ac8aed8b86b
Ryan Phung <ryan.phung@gmail.com>
2016-04-03 14:57:43 +0800

    Linking to template Google Sheet

--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@ angular.module('app').config([
     '$routeProvider', 'TabletopProvider',
     function ($routeProvider, $tabletopProvider) {    
     $tabletopProvider.setTabletopOptions({
-        key: "https://docs.google.com/spreadsheets/d/18k_xx-K2UyOLkZUz3C-qf840aNaab1B6xElDdszeMS8/pubhtml",
+        key: "https://docs.google.com/spreadsheets/d/1NoSg0Sj_ZCcoYfZXMGxP0hcvFtipKUGZ5r0e6xPMrX0/pubhtml",
         prettyColumnNames: false,
         parseNumbers: true
     });
