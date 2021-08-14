0531c4c8041952c408299efc6ea9a5b027ea00a4
Ryan Phung <ryan.phung@gmail.com>
2021-08-14 16:39:08 +0800

    Google API v3 deprecated, hence we need to use another way to download settings data from Google. Using `papaparse` rather than `tabletop`.

--- app/router.js
+++ app/router.js
@@ -1,14 +1,8 @@
 'use strict';
 
 angular.module('app').config([
-    '$routeProvider', 'TabletopProvider',
-    function ($routeProvider, $tabletopProvider) {    
-    $tabletopProvider.setTabletopOptions({
-        key: "https://docs.google.com/spreadsheets/d/1NoSg0Sj_ZCcoYfZXMGxP0hcvFtipKUGZ5r0e6xPMrX0/pubhtml",
-        prettyColumnNames: false,
-        parseNumbers: true
-    });
-    
+    '$routeProvider',
+    function ($routeProvider) {
     $routeProvider
         .when("/", { // Home
             templateUrl: "app/partials/main.html", controller: "MainController"
--- app/services/server.js
+++ app/services/server.js
@@ -1,12 +1,46 @@
 'use strict';
 
-angular.module('app').factory('ServerService', ['$http', 'UiBasicService', '$q', 'Tabletop', function($http, $uiBasic, $q, $tabletop) {
+const configGoogleSheetId = '1NoSg0Sj_ZCcoYfZXMGxP0hcvFtipKUGZ5r0e6xPMrX0'
+const sheets = [
+  'Groups',
+  'Activities',
+  'Baselines',
+  'Result Interpretation',
+  'Settings'
+]
+
+angular.module('app').factory('ServerService', ['$http', 'UiBasicService', '$q', 'Papa', function($http, $uiBasic, $q, $papa) {
     return {
         loadData: function() {
             return $q(function (resolve, reject) {
-                $tabletop.then(function(data) {
-                    resolve(data[1]);
-                });
+                Promise.all(sheets.map(function(sheet) {
+                  return new Promise(function(resolve, reject) {
+                    const url = `https://docs.google.com/spreadsheets/d/${configGoogleSheetId}/gviz/tq?tqx=out:csv&sheet=${encodeURI(sheet)}&headers=1`
+                    console.log(`Downloading sheet ${sheet} from ${url}`)
+
+
+                    $papa.parse(url, {
+                      download: true,
+                      header: true,
+                      transformHeader:function(h) {
+                        return h.replace(/[^a-zA-Z0-9]/g, '') // remove special characters
+                          .toLowerCase()//.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()) // to camel case
+                      },
+                      complete: function(results) {
+                        var data = results.data
+                        resolve(data)
+                      }
+                    })
+                  })
+                })
+              ).then(function(results) {
+                const resultsMap = sheets.reduce(function(s, sheet, i) {
+                  s[sheet] = results[i]
+                  return s
+                }, {})
+
+                resolve(resultsMap)
+              }).catch(reject)
             });
         }
     };
